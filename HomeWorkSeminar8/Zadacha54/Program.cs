49fbf50bd3c6d816d1abecf81516d2cde9a5662e
kirill <kirillbalamut@tut.by>
2023-07-05 00:40:06 +0300

    Feat: add home work seminar 8 tasks 54 & 56

---
+++ HomeWorkSeminar8/Zadacha54/Program.cs
@@ -0,0 +1,75 @@
+﻿/*Задача 54: Задайте двумерный массив. Напишите программу, 
+которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4 
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+*/
+const int row = 5;
+const int col = 5;
+
+int[,] array = new int[5, 5];
+Console.WriteLine("Исходный массив : ");
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(1, 10);
+        Console.Write(array[i, j] + " ");
+
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("упорядоченный массив на увеличение: ");
+
+for (int i = 0; i < row; i++)
+{
+    var stack = new Stack<int>();
+
+    for (int j = 0; j < col - 1; j++)
+        for (int k = j + 1; k < col; k++)
+        {
+            if (array[i, k] < array[i, j])
+            {
+                stack.Push(array[i, k]);
+                array[i, k] = array[i, j];
+                array[i, j] = stack.Pop();
+            }
+        }
+}
+
+for (int i = 0; i < row; i++)
+{
+    for (int j = 0; j < col; j++)
+        Console.Write(array[i, j].ToString() + ' ');
+    Console.WriteLine("");
+}
+Console.WriteLine("упорядоченный массив на уменьшение : ");
+
+for (int i = 0; i < row; i++)
+{
+    var stack = new Stack<int>();
+
+    for (int j = 0; j < col + 1; j++)
+        for (int k = j + 1; k < col; k++)
+        {
+            if (array[i, k] > array[i, j])
+            {
+                stack.Push(array[i, k]);
+                array[i, k] = array[i, j];
+                array[i, j] = stack.Pop();
+            }
+        }
+}
+
+for (int i = 0; i < row; i++)
+{
+    for (int j = 0; j < col; j++)
+        Console.Write(array[i, j].ToString() + ' ');
+    Console.WriteLine("");
+}
---
+++ HomeWorkSeminar8/Zadacha56/Program.cs
@@ -0,0 +1,71 @@
+﻿/*
+
+Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей 
+суммой элементов.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+*/
+Console.Write("введите количество строк : ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите количество столбцов : ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] randomArray = new int[m, n];
+
+void mas(int n, int m)
+{
+    int i, j;
+    Random rand = new Random();
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < m; j++)
+        {
+            randomArray[i, j] = rand.Next(1, 9);
+        }
+    }
+}
+
+
+void printm(int[,] array)
+{
+    int i, j;
+    for (i = 0; i < array.GetLength(0); i++)
+    {
+        Console.WriteLine();
+        for (j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+mas(m, n);
+Console.WriteLine("Исходный массив: ");
+printm(randomArray);
+
+// Функция, считающая сумму элементов в строке
+int SumLine(int[,] array, int i)
+{
+    int sum = array[i, 0];
+    for (int j = 1; j < array.GetLength(1); j++)
+    {
+        sum += array[i, j];
+    }
+    return sum;
+}
+
+int minSum = 1;
+int sum = SumLine(randomArray, 0);
+for (int i = 1; i < randomArray.GetLength(0); i++)
+{
+    if (sum > SumLine(randomArray, i))
+    {
+        sum = SumLine(randomArray, i);
+        minSum = i + 1;
+    }
+}
+Console.WriteLine($" Строка c наименьшей суммой элементов: {minSum}");
