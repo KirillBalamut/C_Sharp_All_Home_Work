ba11daf6ad206d08e038c37f23a21d2890b57d35
KirillBalamut <KirillBalamut@tut.by>
2023-07-17 13:44:06 +0300

    feat: add zadaha 68

---
+++ HomeWorkSeminar9/Zadaha68/Program.cs
@@ -0,0 +1,62 @@
+﻿/*Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29
+*/
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+/*
+int UserRequest(string message)
+{
+    Console.Write(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+*/
+AkkermanFunction(m, n);
+
+// вызов функции Аккермана
+void AkkermanFunction(int m, int n)
+{
+    Console.Write(Akkerman(m, n));
+}
+
+// функция Аккермана
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return Akkerman(m - 1, 1);
+    }
+    else
+    {
+        return (Akkerman(m - 1, Akkerman(m, n - 1)));
+    }
+}
+/*
+int m = UserRequest("Задайте значение M: ");
+int n = UserRequest("Задайте значение N: ");
+*/
+/*
+int Prompt(string message)
+{
+  Console.Write(message);
+  int result = Convert.ToInt32(Console.ReadLine());
+  return result;
+}
+int Akkerman(int m, int n)
+{
+  if (m == 0) return n + 1;
+  if (m > 0 && n == 0) return Akkerman(m - 1, 1);
+  else return Akkerman(m - 1, Akkerman(m, n - 1));
+}
+int m = Prompt("Задайте значение M: ");
+int n = Prompt("Задайте значение N: ");
+Console.WriteLine($"A({m},{n}) = {Akkerman(m, n)}");
+*/
